6a9f3ce1b6ff3660d00b30a646a3f0ebb64f45f9
maxth <maxthalehim@icloud.com>
2023-11-30 21:24:49 +0100

    test

--- src/main/kotlin/max/Main.kt
+++ src/main/kotlin/max/Main.kt
@@ -1,7 +1,7 @@
 package max
 
 fun main(args: Array<String>) {
-    println("Hello Worgd!")
+    println("Hello Worgdd!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
